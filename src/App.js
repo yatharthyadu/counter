4d9835e29300802a717cafe5e612f7abab4c3b98
yatharthyadu <91853007+yatharthyadu@users.noreply.github.com>
2022-05-21 01:26:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import { Category } from '../cmponents/category';
 
 function App() {
+  const [count,setCount] = useState(2);
+
+  const handleChange = (value)=>{
+    setCount(count+value);
+  }
+
+  const handleDouble = (value)=>{
+    setCount(count*value);
+  }
+  if(count>=100){
+    return <h4>Coumter has reached max val</h4>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yatharth's Counter app</h1>
+      <h3>Counter:{count}</h3>
+      <button onClick={()=>{
+        if(count>=20){
+          return;
+        }
+        // counter++;
+        handleChange(1)   //handle called here
+        console.log("updating")
+      }}>Add</button>
+
+      <button onClick={()=>{
+        // counter++;
+        handleChange(-1) 
+        console.log("updating")
+      }}>Substract</button>
+
+      <button onClick={()=>{
+        // counter++;
+        handleDouble(2) 
+        console.log("updating")
+      }}>Double</button>
+      <div className={count % 2 === 0 ? 'green' : 'red'}>Number is:{count%2===0 ? "Even": "Odd"}</div>
+      
     </div>
   );
 }
